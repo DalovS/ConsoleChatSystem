852a6e7fb42352331ef11797203cabbc2b4b03ba
Sabri Dalov <123773953+DalovS@users.noreply.github.com>
2023-09-20 23:06:23 +0300

    Add files via upload

---
+++ ChatConsole/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    private static List<ClientHandler> clients = new List<ClientHandler>();
+
+    static void Main()
+    {
+        TcpListener server = null;
+        try
+        {
+            // Задаване на IP адрес и порт за сървъра
+            int port = 12345;
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+
+            // Създаване на TcpListener за слушане на входящи връзки
+            server = new TcpListener(ipAddress, port);
+            server.Start();
+
+            Console.WriteLine("Сървърът е стартиран и чака за клиенти...");
+
+            while (true)
+            {
+                // Приемане на връзка от клиент
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Клиент свързан.");
+
+                // Стартиране на нова нишка за обработка на клиента
+                ClientHandler clientHandler = new ClientHandler(client);
+                clients.Add(clientHandler);
+                Thread clientThread = new Thread(clientHandler.Handle);
+                clientThread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Грешка: {ex}");
+        }
+        finally
+        {
+            server?.Stop();
+        }
+    }
+
+    // Клас за обработка на връзката с клиент
+    class ClientHandler
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private string clientName;
+
+        public ClientHandler(TcpClient client)
+        {
+            this.client = client;
+        }
+
+        public void Handle()
+        {
+            try
+            {
+                stream = client.GetStream();
+                byte[] buffer = new byte[4096]; // Увеличете размера на буфера
+
+                // Вземете името на клиента (може да го изпратите от клиента преди това)
+                byte[] nameBuffer = new byte[4096];
+                int nameBytesRead = stream.Read(nameBuffer, 0, nameBuffer.Length);
+                clientName = Encoding.UTF8.GetString(nameBuffer, 0, nameBytesRead);
+
+               
+
+                while (true)
+                {
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    if (bytesRead <= 0)
+                    {
+                        break;
+                    }
+
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                    // Изпратете съобщението на всички клиенти, освен на текущия
+                    SendToOthers($"{clientName}: {message}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Грешка при обслужване на клиент: {ex}");
+            }
+            finally
+            {
+                // Известете всички клиенти, че този клиент напуска чата
+                clients.Remove(this);
+                SendToAll($"{clientName} напусна чата.");
+                client.Close();
+            }
+        }
+
+        private void SendToAll(string message)
+        {
+            foreach (var clientHandler in clients)
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                clientHandler.stream.Write(data, 0, data.Length);
+                clientHandler.stream.Flush();
+            }
+        }
+
+        private void SendToOthers(string message)
+        {
+            foreach (var clientHandler in clients)
+            {
+                if (clientHandler != this)
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    clientHandler.stream.Write(data, 0, data.Length);
+                    clientHandler.stream.Flush();
+                }
+            }
+        }
+    }
+}
---
+++ ChatConsoleClientAnother/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Client
+{
+    static void Main()
+    {
+        try
+        {
+            // Задаване на IP адрес и порт за сървъра
+            int port = 12345;
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            TcpClient client = new TcpClient();
+            // Свързване към сървъра
+            client.Connect(ipAddress, port);
+            Console.WriteLine("Свързан със сървъра.");
+            NetworkStream stream = client.GetStream();
+            Console.Write("Въведете вашето име: ");
+            string clientName = Console.ReadLine();
+            byte[] nameData = Encoding.UTF8.GetBytes(clientName);
+            stream.Write(nameData, 0, nameData.Length);
+            // Нишка за приемане на съобщения от сървъра и изписване на конзолата
+            Thread receiveThread = new Thread(() =>
+            {
+                byte[] data = new byte[4096];
+                int bytesRead;
+                while (true)
+                {
+                    try
+                    {
+                        bytesRead = stream.Read(data, 0, data.Length);
+                        if (bytesRead <= 0)
+                        {
+                            break;
+                        }
+                        string message = Encoding.UTF8.GetString(data, 0, bytesRead);
+                        Console.WriteLine(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Грешка при приемане на съобщение: {ex}");
+                        break;
+                    }
+                }
+            });
+            receiveThread.Start();
+            // Нишка за изпращане на съобщения
+            Thread sendThread = new Thread(() =>
+            {
+                while (true)
+                {
+                    // Четене на съобщение от конзолата и изпращане на сървъра                  
+                    string message = Console.ReadLine();                 
+                    byte[] messageData = Encoding.UTF8.GetBytes(message);
+                    stream.Write(messageData, 0, messageData.Length);
+                }
+            });
+            sendThread.Start();
+            // Изчакване на завършването на нишките
+            receiveThread.Join();
+            sendThread.Join();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Грешка: {ex}");
+        }
+    }
+}
